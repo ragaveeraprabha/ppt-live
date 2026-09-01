b86d61d193e3775e2d0d916814cffd615ff3f8ae
ragaveeraprabha <ragavikamalesh2005@gmail.com>
2026-09-01 10:37:59 +0530

    Configure server network and realtime ports

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,8 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   distDir: process.env.NEXT_DIST_DIR || ".next",
+
+  allowedDevOrigins: ["192.168.68.58"],
 };
 
 export default nextConfig;
